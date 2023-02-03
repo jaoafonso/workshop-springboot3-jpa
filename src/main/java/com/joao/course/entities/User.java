7266ce7d1786c3f66cda2cc5243d5a8252e74e99
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-03 18:14:57 -0300

    Banco de dados H2, perfil de testes e JPA

--- src/main/java/com/joao/course/entities/User.java
+++ src/main/java/com/joao/course/entities/User.java
@@ -1,14 +1,20 @@
 package com.joao.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 4504089150055095107L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
