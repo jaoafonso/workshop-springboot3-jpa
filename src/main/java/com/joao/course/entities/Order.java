dc90418f2a8f64e2bed2b9eaf197e7a46e39aaa3
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-06 18:37:21 -0300

    Enum OrderStatus implementado

--- src/main/java/com/joao/course/entities/Order.java
+++ src/main/java/com/joao/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.joao.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.joao.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -22,6 +23,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -29,11 +32,12 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
-    }
+    }   
 
     public Long getId() {
         return id;
@@ -49,6 +53,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
