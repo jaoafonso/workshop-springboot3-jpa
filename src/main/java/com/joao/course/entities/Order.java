c788c17e94de1e6c0573f7a539d190f58b5acfb5
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-07 17:36:02 -0300

    Métodos subTotal e Total implementados

--- src/main/java/com/joao/course/entities/Order.java
+++ src/main/java/com/joao/course/entities/Order.java
@@ -93,6 +93,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        double total = 0.0;
+        for (OrderItem item : items) {
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
