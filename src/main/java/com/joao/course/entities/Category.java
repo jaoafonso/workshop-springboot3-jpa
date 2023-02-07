dc092afda6e6e5c38350e48674aa5f81a255a30c
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-06 21:25:22 -0300

    Product e associação ManyToMany com JoinTable implementados

--- src/main/java/com/joao/course/entities/Category.java
+++ src/main/java/com/joao/course/entities/Category.java
@@ -1,10 +1,13 @@
 package com.joao.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -17,6 +20,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -40,6 +47,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/joao/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.joao.course.repositories;
+
+import com.joao.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
